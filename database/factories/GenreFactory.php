4965edc02f4d26f7cd9c774d17b2bb4ecebc7c43
rakib <phi.rakib@gmail.com>
2024-04-29 19:22:24 +0600

    added genre factory & seeder code

--- database/factories/GenreFactory.php
+++ database/factories/GenreFactory.php
@@ -17,7 +17,7 @@ class GenreFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->sentence(2)
         ];
     }
 }
--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Genre;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class GenreSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Genre::factory()->count(250)->create();
     }
 }
