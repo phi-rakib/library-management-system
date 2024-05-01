fdb3dab8e0964e389ff4fac97b7fa5fbb14357f0
rakib <phi.rakib@gmail.com>
2024-05-01 12:06:44 +0600

    modified borrow table

--- app/Filament/Resources/BorrowResource.php
+++ app/Filament/Resources/BorrowResource.php
@@ -50,10 +50,7 @@ class BorrowResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('user_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('book_id')
+                Tables\Columns\TextColumn::make('user.name')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('borrowed_at')
